5e5fa92d8be1e26d5da45f1b2bff7dba5c815263
Bastien Sevajol <bastien.sevajol@algoo.fr>
2022-05-02 21:26:19 +0200

    configure and manager bon by config

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,46 @@ mod linux;
 #[cfg(target_os = "windows")]
 mod windows;
 
+mod config;
+mod error;
+
 #[derive(StructOpt, Debug)]
 #[structopt(name = "basic")]
 pub struct Opt {
-    manager_bin_path: String,
-    configure_bin_path: String,
+    #[structopt(short, long = "--manager-bin-path")]
+    manager_bin_path: Option<String>,
+    #[structopt(short, long = "--configure-bin-path")]
+    configure_bin_path: Option<String>,
 }
 
 fn main() {
     let opt = Opt::from_args();
+    let config = match config::Config::from_env() {
+        Ok(config_) => config_,
+        Err(error) => {
+            eprintln!("{:?}", error);
+            std::process::exit(1);
+        }
+    };
 
-    println!("Run {}", &opt.manager_bin_path);
-    let mut manager_child = Command::new(&opt.manager_bin_path).spawn().unwrap();
+    let trsync_manager_bin_path = if let Some(trsync_manager_bin_path_) = opt.manager_bin_path {
+        trsync_manager_bin_path_.clone()
+    } else {
+        config.trsync_manager_bin_path.clone()
+    };
+
+    let trsync_manager_configure_bin_path =
+        if let Some(trsync_manager_configure_bin_path_) = opt.configure_bin_path {
+            trsync_manager_configure_bin_path_.clone()
+        } else {
+            config.trsync_manager_configure_bin_path.clone()
+        };
+
+    let mut manager_child = Command::new(trsync_manager_bin_path).spawn().unwrap();
 
     #[cfg(target_os = "linux")]
     {
-        match linux::run_tray(opt.configure_bin_path.clone()) {
+        match linux::run_tray(trsync_manager_configure_bin_path.clone()) {
             Err(error) => {
                 eprintln!("{}", error)
             }
@@ -32,7 +56,7 @@ fn main() {
 
     #[cfg(target_os = "windows")]
     {
-        match windows::run_tray(opt.configure_bin_path.clone()) {
+        match windows::run_tray(trsync_manager_configure_bin_path.clone()) {
             Err(error) => {
                 eprintln!("{}", error)
             }
